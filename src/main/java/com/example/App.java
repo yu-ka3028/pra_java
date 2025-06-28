6bd5e34b363d60fca4076f73dd1fec1f37829643
ゆーか <yuka.pooh3028@gmail.com>
2025-06-28 14:04:02 +0900

    ドットインストール_はじめてのJava

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,22 +1,52 @@
 package com.example;
-
+import java.util.Scanner;
+import java.util.Random;
 /**
  * メインアプリケーションクラス
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Java Development Environment!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.println("Java home: " + System.getProperty("java.home"));
+        // System.out.println("Hello, Java Development Environment!");
+        // System.out.println("Java version: " + System.getProperty("java.version"));
+        // System.out.println("Java home: " + System.getProperty("java.home"));
+        
+        //変数の扱い方
+        // String name = "Taro";
+        // System.out.println("Hello," + name);
+        // System.out.println("Nice!!");
+
+        // System.out.print("Hello," + name);
+        // System.out.println("Nice!!");
+
+        Integer count = 0;
+        //答えは8で固定
+        // Integer answer = 8;
+        Integer answer = new Random().nextInt(10) + 1;
+
+        while (true){
+            // ユーザーからの入力をそのまま取得
+            // System.out.print("Your guess:" + guess);
+
+            //1.答えと一致したら正解、不正解なら残念と表示
+            //2.guessに対してanswerが大きいか小さいかを判断して表示
+            //3.正解するまでループする
+
+            System.out.print("Your guess?(1-10) ");
+            count++;
+            Integer guess = new Scanner(System.in).nextInt();
+            
+            if (guess == answer){
+                System.out.println("正解" + "Answer: " + answer + "\n" + "試行回数: " + count);
+                break;
+            } 
+            else if (guess < answer){
+                System.out.println("残念!!" + "Your guess:" + guess + "\n" + "もっと大きいです");
+            }
+
+            else {
+                System.out.println("残念!!" + "Your guess:" + guess + "\n" + "もっと小さいです");
+            }
+        }
     }
     
-    /**
-     * 簡単な計算を行うメソッド
-     * @param a 最初の数値
-     * @param b 2番目の数値
-     * @return 合計
-     */
-    public static int add(int a, int b) {
-        return a + b;
-    }
 } 
